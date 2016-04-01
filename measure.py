d6212b4aad4ab591d9df80cffa076b78a4cf22b1
awdesh <awdesh.sharma1@gmail.com>
2016-03-31 22:05:47 -0700

    adding output comment

--- measure.py
+++ measure.py
@@ -9,6 +9,10 @@ import mongoengine as db
 db.connect("test-dicts")
 
 # Example to display usage of pymongo vs MongoEngine.
+# Output-:
+# pymongo took 0.26s
+# ('mongoengine took', 21.69879698753357)
+
 
 # It'll create a db "test-dicts" with collection name "my_model".
 # Below example will create 5000 sample data.
@@ -28,7 +32,7 @@ m = MyModel()
 tree = lambda: defaultdict(tree)  # http://stackoverflow.com/a/19189366/3271558
 data = tree()
 for _d1, _d2, _d3 in itertools.product(data_1, data_2, data_3):
-    data[_d1][_d2][_d3] = list(random.sample(range(5000), 2000))
+    data[_d1][_d2][_d3] = list(random.sample(range(50000), 20000))
 m.data_dict_1 = data
 m.save()
 
